054a236a5891d36b262373885624c1f89e812cee
Chetan Tekam <tekamchetan3500@gmail.com>
2026-02-14 04:36:21 +0530

    feat: add Advanced Logging

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
 #!/usr/bin/env python
 
+# import the logging module
+import logging
+
 # import the api_client module
+from src.logger_config import setup_logger
 from src.api_client import call_github_api
+
+# setup the logger
+setup_logger()
+# get the logger
+logger = logging.getLogger(__name__)
 
 # define a main function to call the GitHub API and print the response
 def main():
+    # log the start of the application
+    logger.info("Starting the application")
+
     status_code, response = call_github_api()
+
+    # log the response
     if status_code is None:
-        print("Error: ", response)
+        logger.error(f"API call failed with error: {response}")
     else:
-        print("Status Code:", status_code)
-        print("Response Body:", response)
+        logger.info(f"API call successful with status code: {status_code}")
+        logger.debug(f"Response Body: {response}")
+
+    # log the end of the application
+    logger.info("Finished the application")
 
 # call the main function
 if __name__ == "__main__":
